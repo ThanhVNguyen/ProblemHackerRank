336d101316b1c28dd951b019081f2ab194af0471
Thanh Nguyen <thanhvnguyen3979@gmail.com>
2020-10-05 18:15:04 +0700

    solution of bill division

---
+++ billDivision.js
@@ -0,0 +1,16 @@
+function bonAppetit(bill, k, b) {
+  let sum = 0;
+  for (let i = 0; i < bill.length; i += 1) {
+    sum += bill[i];
+  }
+  const billAnna = sum - bill[k];
+  const billThem = billAnna / 2;
+  if (billThem < b) {
+    const payMoney = b - billThem;
+    console.log(payMoney);
+  }
+  if (billThem === b) {
+    console.log('Bon Appetit');
+  }
+}
+console.log(bonAppetit([3, 10, 2, 9], 1, 7));
